c7def72f336081778bb4c24b7dac86b7186270be
KWUST <KWUST@DESKTOP-LTGP885>
2025-07-10 18:03:26 +0300

    Add project files.

---
+++ Form2.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DetermineButton = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.FullNameTextBox = New System.Windows.Forms.TextBox()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(45, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name:"
+        '
+        'DetermineButton
+        '
+        Me.DetermineButton.Location = New System.Drawing.Point(184, 128)
+        Me.DetermineButton.Name = "DetermineButton"
+        Me.DetermineButton.Size = New System.Drawing.Size(144, 23)
+        Me.DetermineButton.TabIndex = 1
+        Me.DetermineButton.Text = "Determine First Name"
+        Me.DetermineButton.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(48, 217)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "First Name:"
+        '
+        'FullNameTextBox
+        '
+        Me.FullNameTextBox.Location = New System.Drawing.Point(359, 62)
+        Me.FullNameTextBox.Name = "FullNameTextBox"
+        Me.FullNameTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.FullNameTextBox.TabIndex = 3
+        '
+        'FirstNameTextBox
+        '
+        Me.FirstNameTextBox.Location = New System.Drawing.Point(359, 217)
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        Me.FirstNameTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.FirstNameTextBox.TabIndex = 4
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.FirstNameTextBox)
+        Me.Controls.Add(Me.FullNameTextBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DetermineButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DetermineButton As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents FullNameTextBox As TextBox
+    Friend WithEvents FirstNameTextBox As TextBox
+End Class
---
+++ Form2.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form2
+
+    Private Sub DetermineButton_Click(ByVal sender As Object, ByVale As EventArgs)
+        Dim name As String
+        name = FullNameTextBox.Text
+        Call FirstName(name)
+    End Sub
+    Private Sub FirstName(ByVal name As String)
+        Dim firstSpace As Integer
+        firstSpace = name.IndexOf(" ")
+        FirstNameTextBox.Text = name.Substring(0, firstSpace)
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
